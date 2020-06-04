5d1d2d15124362af9f1f0aa688a261598b17f42c
1A1exander <xalexandr92@mail.ru>
2020-06-04 13:27:20 +0300

    new

---
+++ build/js/script.js
@@ -0,0 +1,52 @@
+
+$(document).ready(function(){
+    $(".circle").circularProgress({
+        color: "#8583e1",
+        line_width: 1,
+        height: "265px",
+        width: "265px",
+        background: "red",
+        percent: 0,
+        starting_position: 0,
+        
+    }).circularProgress('animate', 20, 2500);
+});
+$(".circle2").circularProgress({
+    color: "#25ffe4",
+    line_width: 6,
+    height: "265px",
+    width: "265px",
+    percent: 0,
+
+    // counter_clockwise: true,
+    starting_position: 0
+}).circularProgress('animate', 100, 2000);
+$(".circle3").circularProgress({
+    color: "#25ffe4",
+    line_width: 6,
+    height: "265px",
+    width: "265px",
+    percent: 0,
+
+    // counter_clockwise: true,
+    starting_position: 0
+}).circularProgress('animate', 100, 2000);
+$(".circle4").circularProgress({
+    color: "#25ffe4",
+    line_width: 6,
+    height: "265px",
+    width: "265px",
+    percent: 0,
+
+    // counter_clockwise: true,
+    starting_position: 0
+}).circularProgress('animate', 100, 2000);
+
+new SimpleBar(document.getElementById('myEl'),{
+    scrollbarMinSize: 50
+    
+});
+new SimpleBar(document.getElementById('myEl1'),{
+   
+    scrollbarMaxSize: 50
+});
---
+++ smartgrid.js
@@ -0,0 +1,26 @@
+module.exports = {
+	columns: 12,
+	offset: "30px",
+	
+	container: {
+        maxWidth: "1521",
+        fields: "30px"
+    },
+	breakPoints: {
+		md: {
+            width: "1255px",
+            fields: "15px"
+        },
+        sm: {
+            width: "920pxpx"
+        },
+        xs: {
+            width: "720px"
+        },
+        xxs: {
+            width: "576px",
+           
+        }
+	},
+
+};
